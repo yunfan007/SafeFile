97223a3f5763f0771d9457748a18acb6c44d32df
yunfan007 <107984149+yunfan007@users.noreply.github.com>
2026-07-31 10:06:16 +0800

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 有关程序集的一般信息由以下
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("SafeFile")]
+[assembly: AssemblyDescription("SafeFile(文件保)是一款优秀的文件加密软件，使用独家研制的SageFile标准加密文件格式，有效防护文件泄露。")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("云帆软件工作室")]
+[assembly: AssemblyProduct("SafeFile 1.0.0")]
+[assembly: AssemblyCopyright("Copyright YunfanSoftwareStudio ©  2024")]
+[assembly: AssemblyTrademark("YunfanSoftwareStudio")]
+[assembly: AssemblyCulture("")]
+
+// 将 ComVisible 设置为 false 会使此程序集中的类型
+//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
+//请将此类型的 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("0099fa55-928b-42d0-88ea-3c492a8d5fed")]
+
+// 程序集的版本信息由下列四个值组成: 
+//
+//      主版本
+//      次版本
+//      生成号
+//      修订号
+//
+//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
+//通过使用 "*"，如下所示:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
